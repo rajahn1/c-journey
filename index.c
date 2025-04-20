e52957c14288ee88d63f94c049621bdba07d3980
Rafael <rafayuno@gmail.com>
2025-04-20 01:05:16 -0300

    .

--- index.c
+++ index.c
@@ -2,15 +2,24 @@
 const int DIMENSIONAL_BOX = 166;
 
 int main(){
-    float profit, loss, total, weigth, volume;
-    int height, width;
+    float profit, loss, total;
+    int height, width, length, volume, weight;
+
+    height = 8;
+    length = 12;
+    width = 10;
+
+    volume = height * length * width;
+    weight = (volume + 165) / DIMENSIONAL_BOX;
+
+    printf("Dimensions %dx%dx%d\n", length, width, height);
+    printf("Volume (cubic inch) %d\n", volume);
+    printf("Weight (pounds) %d\n", weight);
 
     total = 0.00f;
     profit = 6420.29f;
     loss = 3240.30f;
 
-
-    total = profit - loss;
-    printf("You have: $ %.2f", total);
+    return 0;
 }
 
