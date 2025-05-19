7fa65c23ab523af88da956e6ee7e679ff9eeb58e
Rafael Jahn <rafayuno@gmail.com>
2025-05-18 22:13:44 -0300

    .

---
+++ 3-formatting-input-output/index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int i ;
+    float x;
+    i = 40;
+    x = 839.21f;
+    //printf("%d|%5d %-5d|%5.3d|\n \t", i, i, i, i);
+    //printf("%10.3f %10.3e|%-10g|\n \t", x, x, x);
+
+
+    printf ("Item\tUnit\tPurchase\n\tPrice\tDate\n");
+    printf(" abraham lincon said: \"Hello World\"\n");
+    
+    printf("Use this symbol: \\ to escape sequences\n ");
+        
+    return 0;
+}
